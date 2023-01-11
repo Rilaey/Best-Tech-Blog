95585392fbc5b7d5035c8515823c53e32cfd096c
Riley <94302477+Rilaey@users.noreply.github.com>
2023-01-10 19:39:52 -0500

    created views folder and multiple templates

---
+++ Develop/controller/homeRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const { User, Blogs } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const blogs = await Blogs.findAll({
+      include: [{ model: User }],
+    });
+    const allBlogs = blogs.map((x) => x.get({ plain: true }));
+
+    res.render("home", {
+      allBlogs,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
