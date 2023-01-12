185d000be7682eca426e69491739a772f935226b
Riley <94302477+Rilaey@users.noreply.github.com>
2023-01-12 18:08:11 -0500

    started working on dashboard template

--- Develop/controller/homeRoutes.js
+++ Develop/controller/homeRoutes.js
@@ -1,15 +1,18 @@
 const router = require("express").Router();
 const { User, Blogs } = require("../models");
+const userAuth = require('../utils/auth')
 
+// home page with existing blogs
 router.get("/", async (req, res) => {
   try {
     const blogs = await Blogs.findAll({
       include: [{ model: User }],
     });
     const allBlogs = blogs.map((x) => x.get({ plain: true }));
-    console.log(allBlogs)
+    //console.log(allBlogs);
     res.render("home", {
       allBlogs,
+      logged_in: req.session.logged_in 
     });
   } catch (err) {
     console.log(err);
@@ -17,5 +20,43 @@ router.get("/", async (req, res) => {
   }
 });
 
+// login page
+router.get("/login", (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-module.exports = router
+// signup page
+router.get("/signup", (req, res) => {
+  try {
+    res.render("signup")
+  } catch(err) {
+    console.log(err);
+    res.status(500).json(err)
+  }
+})
+
+// dashboard
+router.get("/dashboard", (req, res) => {
+  try {
+    res.render("dashboard")
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
+
+// if user is logged in, redirect to dashboard
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,46 +1,47 @@
 const path = require('path');
 const express = require('express');
-//const session = require('express-session');
-const { engine } = require('express-handlebars');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controller');
-//const helpers = require('./utils/helpers');
+const auth = require('./utils/auth');
+const time = require('./utils/time')
 
 const sequelize = require('./config/connection');
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ auth, time });
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {
-//     maxAge: 300000,
-//     httpOnly: true,
-//     secure: false,
-//     sameSite: 'strict',
-//   },
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-//app.use(session(sess));
+app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-app.engine('handlebars', engine());
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set('views', './views');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
