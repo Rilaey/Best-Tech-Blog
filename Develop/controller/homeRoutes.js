03b453880da0d72c9724e1f3115f65715053faff
Riley <94302477+Rilaey@users.noreply.github.com>
2023-01-15 10:12:00 -0500

    login function working, but still working on a bug that still redirects you to the login screen even though user is logged in.

--- Develop/controller/homeRoutes.js
+++ Develop/controller/homeRoutes.js
@@ -30,33 +30,24 @@ router.get("/login", (req, res) => {
   }
 });
 
-// signup page
-router.get("/signup", (req, res) => {
-  try {
-    res.render("signup")
-  } catch(err) {
-    console.log(err);
-    res.status(500).json(err)
-  }
-})
-
 // dashboard
-router.get("/dashboard", (req, res) => {
+router.get('/dashboard', userAuth ,async (req, res) => {
   try {
-    res.render("dashboard")
-  } catch(err) {
-    res.status(500).json(err)
-  }
-})
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Blogs }],
+    });
 
-// if user is logged in, redirect to dashboard
-router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/dashboard');
-    return;
-  }
+    const user = userData.get({ plain: true });
 
-  res.render('login');
+    res.render('dashboard', {
+      user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
